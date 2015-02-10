0e810ce2187cd6bd2fb473b4a293fd45b0dc1e12
Yu Jiang <yjiang26@gmail.com>
2015-02-09 19:18:47 -0500

    revised

---
+++ generated2previous.m
@@ -0,0 +1,6 @@
+load_system('CTADP_lib')
+save_system('CTADP_lib','R2010a\CTADP_lib','ExportToVersion','R2010a');
+
+load_system('CTADP_Demo')
+save_system('CTADP_Demo','R2010a\CTADP_Demo','ExportToVersion','R2010a');
+
