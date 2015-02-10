5c3ceff20c5a5730052dc3d8a216deb9f966b631
Yu Jiang <yjiang26@gmail.com>
2015-02-09 19:24:04 -0500

    adding script to automatically generate previous versions

--- generated2previous.m
+++ generated2previous.m
@@ -1,6 +1,7 @@
 load_system('CTADP_lib')
 save_system('CTADP_lib','R2010a\CTADP_lib','ExportToVersion','R2010a');
+save_system('CTADP_lib','R2014b\CTADP_lib');
 
 load_system('CTADP_Demo')
 save_system('CTADP_Demo','R2010a\CTADP_Demo','ExportToVersion','R2010a');
-
+save_system('CTADP_Demo','R2014b\CTADP_Demo')
